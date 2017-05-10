f37349d3a4fde2d97a39d3dbe1ca3d44183aa95d
Phuseos <Phuseos@gmail.com>
2017-05-10 08:47:30 +0200

    Add project files.

---
+++ AWATO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AWATO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
